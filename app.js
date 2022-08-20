0335460c235470138dc78c1c8246c8bc7950f621
Hernandez-Javier <javihl14@gmail.com>
2022-08-20 16:49:06 -0600

    dashboard fully completed, edit and delete product completed

--- app.js
+++ app.js
@@ -12,10 +12,18 @@ const container_dash = document.querySelector('.dashboard_items');
 function delegate(e) {
   let productsDb = JSON.parse(localStorage.getItem('products'));
   e.preventDefault();
-  localStorage.setItem('view_product', JSON.stringify(productsDb.find(item => item.id == e.target.id)));
-  let test = JSON.parse(localStorage.getItem('view_product'));
-  if (test != null) {
-    window.location.href = './products.html';
+
+  if (e.target.classList[0] == 'edit_button') {
+    localStorage.setItem('product_edit', JSON.stringify(productsDb.find(item => item.id == e.target.id)));
+    window.location.href = './new_product.html';
+  }else if (e.target.classList[0] == 'micar_button_delete'){
+    delete_product(e.target.id);
+  }else{
+    localStorage.setItem('view_product', JSON.stringify(productsDb.find(item => item.id == e.target.id)));
+    let test = JSON.parse(localStorage.getItem('view_product'));
+    if (test != null) {
+      window.location.href = './products.html';
+    }
   }
 }
 
@@ -72,7 +80,36 @@ function login() {
   }
 }
 
+function edit_product() {
+  let product_edit = JSON.parse(localStorage.getItem('product_edit'));
+
+  if (product_edit != null) {
+    document.getElementById('name').value = product_edit.name;
+    document.getElementById('description').textContent = product_edit.description;
+    document.getElementById('image').value = product_edit.image;
+    document.getElementById('lf').textContent = product_edit.lf;
+  }
+  
+}
+
 function new_product() {
+  let id_count = JSON.parse(localStorage.getItem('id_count'));
+  let productsDb = JSON.parse(localStorage.getItem('products'));
+  let product_edit = JSON.parse(localStorage.getItem('product_edit'));
+
+  if (product_edit != null) {
+    //let edited = productsDb.find(item => item.id == product_edit.id);
+    let index = productsDb.findIndex(item => item.id == product_edit.id);
+    productsDb[index].name = $('#name').val();
+    productsDb[index].description = $('#description').val();
+    productsDb[index].image = $('#image').val();
+    productsDb[index].lf = $('#lf').val();
+    localStorage.setItem('products', JSON.stringify(productsDb));
+    window.localStorage.removeItem('product_edit');
+    window.location.href = './dashboard.html';
+    return false;
+  }
+
   const Name = $('#name').val();
   const Description = $('#description').val();
   const Image = $('#image').val();
@@ -80,9 +117,12 @@ function new_product() {
   const Owner = JSON.parse(localStorage.getItem('user_loged')).id;
 
   //insert to a database
-  let productsDb = JSON.parse(localStorage.getItem('products'));
   if(!productsDb) {
     productsDb = [];
+  }
+
+  if(!id_count) {
+    id_count = 0
   }
 
   const product = {
@@ -91,13 +131,14 @@ function new_product() {
     image: Image,
     lf: LF,
     owner: Owner,
-    id: productsDb.length + 1
+    id: id_count + 1
   }
 
   productsDb.push(product);
+  id_count += 1;
   localStorage.setItem('products', JSON.stringify(productsDb));
-  //console.log(JSON.parse(localStorage.getItem('books')));
-  window.location.href = 'dashboard.html';
+  localStorage.setItem('id_count', JSON.stringify(id_count));
+  window.location.href = './dashboard.html';
 }
 
 function charge_all_products() {
@@ -153,9 +194,7 @@ function charge_products() {
       a.textContent = productsDb[i].name;
       a.setAttribute('id', productsDb[i].id);
       bt.setAttribute('id', productsDb[i].id);
-      //bt.addEventListener('click', go_edit_product.pass(bt.id));
-      bt.addEventListener('click', localStorage.setItem('product_edit', JSON.stringify(productsDb.find(item => item.id == bt.id))));
-      //bt2.addEventListener('click', delete_product.bind(productsDb[i].id);
+      bt2.setAttribute('id', productsDb[i].id);
   
       new_div3.appendChild(img);
       new_div4.appendChild(a);
@@ -168,7 +207,7 @@ function charge_products() {
       new_div4.classList.add("dash_product");
       new_div2.classList.add("dash_li");
       img.classList.add("img_style");
-      bt.classList.add("micar_button");
+      bt.classList.add("edit_button");
       bt2.classList.add("micar_button_delete");
       a.classList.add("center_text");
       $('.dashboard_items').prepend(new_li);
@@ -176,17 +215,15 @@ function charge_products() {
   }
 }
 
-function go_edit_product(id) {
-  window.location.href = './edit_product.html';
-}
-
 function delete_product(id) {
   const productsDb = JSON.parse(localStorage.getItem('products'));
-  const index = productsDb.findIndex(item => item.id === id);
+  const index = productsDb.findIndex(item => item.id == id);
+
   if (index > -1) {
     productsDb.splice(index, 1);
   }
   localStorage.setItem('products', JSON.stringify(productsDb));
+  window.location.href = './dashboard.html';
 }
 
 function view_product() {
@@ -197,20 +234,6 @@ function view_product() {
   document.getElementById("owner").textContent= usersDb.find(item => item.id == product.owner).name;
   document.getElementById("description").textContent = product.description;
   document.getElementById("lf").textContent = product.lf;
-
-
-  let new_div = document.createElement("div");
-  let new_div2 = document.createElement("div");
-  let img = document.createElement("img");
-  let bt = document.createElement("button");
-  let new_h1 = document.createElement("h1");
-  let new_h6 = document.createElement("h6");
-  let new_div3 = document.createElement("div");
-  let new_h2 = document.createElement("h2");
-  let new_p = document.createElement("p");
-  let new_h2_2 = document.createElement("h2");
-  let new_p2 = document.createElement("p");
-  let new_div4 = document.createElement("div");
 }
 
 function loged() {
